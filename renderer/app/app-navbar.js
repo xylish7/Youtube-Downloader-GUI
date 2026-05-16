e7c7d686e536b9b2db0e56a8e16f5bcbda3a2e93
Filip Frincu <filipfrincu@gmail.com>
2026-05-16 19:33:59 +0300

    remove irrelevant elements

--- renderer/app/app-navbar.js
+++ renderer/app/app-navbar.js
@@ -3,13 +3,6 @@ const { app } = require("@electron/remote");
 
 // On hover, display general-settings button color
 const $generalSettings = $("#general-settings");
-$generalSettings.on("mouseover", () => {
-  document.getElementById("setting-icon").style.color = "rgb(254, 57, 96)";
-});
-$generalSettings.on("mouseleave", () => {
-  document.getElementById("setting-icon").style.color =
-    "rgba(254, 57, 96, 0.5)";
-});
 
 // Open modal
 const $modal = $(".modal");
